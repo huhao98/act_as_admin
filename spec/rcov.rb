12b7a7699b647dea4557ad0c4fec4098f335fbb4
Hu Hao <huhao98@gmail.com>
2013-04-17 19:07:47 +0800

    modified spec_helper

---
+++ spec/rcov.rb
@@ -0,0 +1,12 @@
+if( ENV['RCOV'] == 'on' )
+  require 'simplecov'
+  require 'simplecov-rcov'
+  class SimpleCov::Formatter::MergedFormatter
+    def format(result)
+      SimpleCov::Formatter::HTMLFormatter.new.format(result)
+      SimpleCov::Formatter::RcovFormatter.new.format(result)
+    end
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+  SimpleCov.start 'test_frameworks'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
-if( ENV['RCOV'] == 'on' )
-  require 'simplecov'
-  require 'simplecov-rcov'
-  class SimpleCov::Formatter::MergedFormatter
-    def format(result)
-      SimpleCov::Formatter::HTMLFormatter.new.format(result)
-      SimpleCov::Formatter::RcovFormatter.new.format(result)
-    end
-  end
-  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-  SimpleCov.start 'test_frameworks'
-end
-
-
+require File.expand_path("../rcov.rb",  __FILE__)
 require File.expand_path("../dummyapp/config/environment.rb",  __FILE__)
-
 require 'mongoid'
 require "action_controller/railtie"
 require 'act_as_admin'
 require 'rspec'
 require 'rspec/rails'
 
+Dir["spec/support/**/*.rb"].each {|f| require File.expand_path(f)}
 RSpec.configure do |config|
   config.order = "random"
 end
