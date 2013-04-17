adc03b0e1a236fbe7dffc30b6772ecaac0c25ab3
Hu Hao <huhao98@gmail.com>
2013-04-17 17:40:46 +0800

    add config to autoload

--- lib/act_as_admin.rb
+++ lib/act_as_admin.rb
@@ -2,6 +2,7 @@ module ActAsAdmin
   autoload :NavConfig, 'act_as_admin/nav_config'
   autoload :Controller, 'act_as_admin/controller'
   autoload :ViewResolver, 'act_as_admin/view_resolver'
+  autoload :Config, 'act_as_admin/config'
   module Helpers
     autoload :PathHelper, 'act_as_admin/helpers/path_helper'
     autoload :NavHelper, 'act_as_admin/helpers/nav_helper'
