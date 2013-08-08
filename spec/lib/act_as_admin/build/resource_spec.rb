6b9466a949cd118ea7728502630d72133badb42c
Hu Hao <huhao98@gmail.com>
2013-08-08 11:47:52 +0800

    removed unused spec

--- spec/lib/act_as_admin/build/resource_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ActAsAdmin::Build::Resource do
-  it "should build controller path" do
-    subject.path("users").to("orders").to("order_items")
-    expect(subject.paths.first.parents.keys).to eq(["users", "orders", "order_items"])
-  end
-  
-end
